c47edaad59d98c5338b521fbe7832f22b5e4e650
faqihfnf <faqih.fnf@gmail.com>
2024-10-22 10:31:26 +0700

    until step 51

--- script.js
+++ script.js
@@ -51,6 +51,12 @@ const playSong = (id) => {
   audio.play();
 };
 
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+  playButton.classList.remove("playing");
+  audio.pause();
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -58,7 +64,7 @@ const renderSongs = (array) => {
     <li
     id="song-${song.id}"
       class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
       <span class= "playlist-song-title">${song.title}</span>
       <span class= "playlist-song-artist">${song.artist}</span>
       <span class= "playlist-song-duration">${song.duration}</span>
@@ -73,7 +79,19 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
-playButton.addEventListener("click", (event) => {});
+const getCurrentSongIndex = () => {
+  return userData?.songs.indexOf(userData?.currentSong);
+};
+
+playButton.addEventListener("click", () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
+  }
+});
+
+pauseButton.addEventListener("click", pauseSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
